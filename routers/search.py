93e4e11c0a8ecc5f65ed46c4a9c184d67d2c6aa0
Bogdan <75094394+F0ggy7@users.noreply.github.com>
2023-07-06 21:16:34 +0300

    fix

--- routers/search.py
+++ routers/search.py
@@ -4,6 +4,6 @@ from services.data_search import get_data_search
 router = APIRouter()
 
 @router.get('/fedresurs/search', description='Get bankrupt information')
-async def get_search(types: int = Query(..., gt=0, le=2, description='Specify the type of person or company(1 or 2)'), searchString: str = Query("", description='Specify what query to search for') , isActiveLegalCase: str = Query("null", description='Indicate the status of the case (true, false, null) Default is null'), regionId: str = Query("",description="You can specify a region ID")):
+async def get_search(types: int = Query(..., gt=0, le=2, description='Specify the type of person or company(2 or 1)'), searchString: str = Query("", description='Specify what query to search for') , isActiveLegalCase: str = Query("null", description='Indicate the status of the case (true, false, null) Default is null'), regionId: str = Query("",description="You can specify a region ID")):
     data = await get_data_search(types, searchString, isActiveLegalCase, regionId)
     return data
