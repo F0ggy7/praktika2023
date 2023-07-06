c296698ceb2dc8d70d5937e1bebb0b6cf14351f2
Bogdan <75094394+F0ggy7@users.noreply.github.com>
2023-07-06 19:55:35 +0300

    fix

--- routers/entity.py
+++ routers/entity.py
@@ -3,7 +3,7 @@ from services.info_entity import info_for_type_entity as InfoEntityService
 
 router = APIRouter()
 
-@router.get('/fedresurs/info/entity')
+@router.get('/fedresurs/info/entity', description='Get information about the type of bankruptcy')
 async def regions():
     type_entity = await InfoEntityService()
     return (type_entity)
